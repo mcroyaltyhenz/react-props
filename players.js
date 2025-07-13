5433545d21228a250f080842a48710bf36cf6fd6
mcroyaltyhenz <henscheljustice@gmail.com>
2025-07-13 08:40:16 +0100

    Create players.js

---
+++ players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentinian",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://img.a.transfermarkt.technology/portrait/big/28003-1671435885.jpg"
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norwegian",
+    jerseyNumber: 9,
+    age: 23,
+    imageUrl: "https://img.a.transfermarkt.technology/portrait/big/418560-1671435885.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "French",
+    jerseyNumber: 7,
+    age: 24,
+    imageUrl: "https://img.a.transfermarkt.technology/portrait/big/342229-1671435885.jpg"
+  },
+  {
+    name: "Kevin De Bruyne",
+    team: "Manchester City",
+    nationality: "Belgian",
+    jerseyNumber: 17,
+    age: 32,
+    imageUrl: "https://img.a.transfermarkt.technology/portrait/big/88755-1671435885.jpg"
+  }
+];
+
+export default players;
